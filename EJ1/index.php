ca6bebea0183c7967960c5b5ecf7a72609886724
jaberber <jaberber@upo.es>
2022-11-20 16:52:09 +0100

    Update index.php

--- EJ1/index.php
+++ EJ1/index.php
@@ -7,7 +7,7 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Ejercicio 1 - EPD7-Grupo-12</title>
+        <title>Ejercicio 1 - EPD7-Grupo-12 (PR)</title>
     </head>
     <body>
         <?php
